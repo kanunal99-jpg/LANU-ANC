5352882192ea59c0e6ecea6261a5513ff3749382
kanunal99-jpg <kanunal99@gmail.com>
2026-09-17 21:38:33 +0300

    build: prepare native AAudio engine module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,6 +13,12 @@ android {
         targetSdk = 35
         versionCode = 1
         versionName = "0.1.0"
+
+        externalNativeBuild {
+            cmake {
+                cppFlags += "-std=c++17 -O2 -DNDEBUG"
+            }
+        }
     }
 
     buildTypes {
@@ -24,6 +30,15 @@ android {
             )
         }
     }
+
+    externalNativeBuild {
+        cmake {
+            path = file("src/main/cpp/CMakeLists.txt")
+            version = "3.22.1"
+        }
+    }
+
+    ndkVersion = "27.2.12479018"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
